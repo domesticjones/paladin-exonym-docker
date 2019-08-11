c3cc3723d5b10d2a5f56352b9ab37ba41914e6c9
Dustin Jones <info@domesticjones.com>
2019-08-10 23:01:20 -0600

    Style default page template for all devices

--- page.php
+++ page.php
@@ -3,7 +3,10 @@
 	get_header();
 		if(have_posts()): while(have_posts()): the_post();
       get_template_part('modules/hero');
-			the_title();
+			echo ex_wrap('start', 'default');
+				echo '<article class="default-content">' . apply_filters('the_content', get_the_content()) . '</article>';
+				echo '<aside class="default-sidebar">' . get_the_post_thumbnail($post->ID, 'medium') . get_field('embed_code') . '</aside>';
+			echo ex_wrap('end');
 		endwhile; endif;
 	get_footer();
 ?>
